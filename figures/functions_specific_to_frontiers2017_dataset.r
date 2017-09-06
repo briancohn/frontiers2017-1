bcc36f6b5feb7de58d231aef5ecd9ddd63c893be
Brian Cohn <bcohn12@gmail.com>
2017-09-06 11:12:18 -0700

    Created minidataset as output in initial ETL #1

---
+++ figures/functions_specific_to_frontiers2017_dataset.r
@@ -0,0 +1,6 @@
+#hardcoded specifically for realTimeData2017_08_16_13_23_42_subset_of_first_posture.csv
+save_snapshot_for_first_posture <- function(raw_data_timeseries_df, output_filepath="~/Resilio Sync/data/realTimeData2017_08_16_13_23_42_subset_of_first_posture.csv") {
+  first_posture_data <- split_by_position(first_data_chunk$adept_x, first_data_chunk)[[1]] #only get first posture
+  forces <- split_by_reference_force(first_posture_data)[2:101]
+  write.csv(first_posture_data, output_filepath)
+}
--- figures/main.r
+++ figures/main.r
@@ -1,6 +1,7 @@
 setwd('~/Documents/GitHub/bc/frontiers2017/figures')
 source('data_description.r')
 source('time_series_functions.r')
+source('functions_specific_to_frontiers2017_dataset.r')
 
 # Define parameters for analysis
 muscle_names = c("M0", "M1", "M2","M3","M4","M5","M6")
@@ -8,10 +9,18 @@ maximum_tendon_force = 20
 minimum_tendon_force = 3
 data_location <- "~/Resilio Sync/data/realTimeData2017_08_16_13_23_42.txt"
   
+#Fig DataDescription
 # Only take first part of data to create data description figure. (60e3 = first 60 seconds)
-first_data_chunk <- read.csv(data_location, nrows=400e3, header=TRUE)
+first_data_chunk <- read.csv(data_location, nrows=85e3, header=TRUE)
+
+
+#Save snapshot of just first posture
+save_snapshot_for_first_posture(raw_data_timeseries_df=first_data_chunk)
+
+
+
+
 
 pdf("data_description_analysis.pdf", width=14.0, height=10)
   data_description_analysis(first_data_chunk, minimum_tendon_force, maximum_tendon_force, indices_of_interest=2:5)
 dev.off()
-
